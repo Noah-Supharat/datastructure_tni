b555afc9a0214cb3b83eb610f4e73820ae77d6bc
Suphanan_S. <noahsuphanan148@gmail.com>
2026-07-24 10:12:47 +0700

    week 4 activity

---
+++ LabSheet04/src/BrowserHistory.java
@@ -0,0 +1,9 @@
+
+public class BrowserHistory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LabSheet04/src/TodoList.java
@@ -0,0 +1,25 @@
+
+public class TodoList {
+	public static void main(String[] args) {
+		DoublyLinkedList todoList = new DoublyLinkedList();
+		
+		todoList.insert("Finished Homework");
+		todoList.insert("Laundry");
+		todoList.insert("Group Meeting");
+		
+		System.out.println("To-Do List = " + todoList.traversal());
+		
+		todoList.insert(0, "Submit Report");
+		System.out.println("To-Do List = " + todoList.traversal());
+		
+		todoList.insert(2, "Buy Food");
+		System.out.println("To-Do List = " + todoList.traversal());
+		
+		todoList.insert("Go to Gym");
+		System.out.println("To-Do List = " + todoList.traversal());
+		
+		System.out.println();
+		
+		System.out.println("Backward Traversal = " + todoList.backwardTraversal());
+	}
+}
